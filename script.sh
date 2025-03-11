6a2ab1ab3f14099b1939e9bf8a06b880264c16b6
edurekadevops89 <edurekadevops89@gmail.com>
2025-03-11 00:42:23 +0000

    b1gui

--- script.sh
+++ script.sh
@@ -4,3 +4,5 @@ pwd
 echo "hey you have suesssfully clonded the repo and excected the script"
 echo " this job has triggred as part of auto trigger"
 echo " second auto trigger demo"
+
+Brnach1 from the remoteGUI
