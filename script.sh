eb729847cb343aed625a3c4a40e7d1700a38b1d0
edurekadevops89 <edurekadevops89@gmail.com>
2025-04-16 20:24:39 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ pwd
 echo "hey you have suesssfully clonded the repo and excected the script"
 echo " this job has triggred as part of auto trigger"
 echo " second auto trigger demo"
+echo " now this is been executed as part of my actions work flow"
 
-Brnach1 from the remoteGUI
+
